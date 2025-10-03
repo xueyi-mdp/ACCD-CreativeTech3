bf20ea1aec28d4dc69f70799240ee9dd48a72b64
xueyi-mdp <xwu13@inside.artcenter.edu>
2025-10-03 14:37:57 -0700

    updated API calls

--- Project/script/script.js
+++ Project/script/script.js
@@ -1,61 +1,32 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-     <header>
-        <h1>Fetch example</h1>
-        <h3>Start here:</h3>
-        <button id="fetch-button">Fetch data</button>   
+  const fetchRegionData=()=>{
+       fetch('https://restcountries.com/v3.1/all')
+       .then(respond=>respond.json())
+       .then(data=>appendRegionData)
+ }
+  const fetchBirdData=(regionCode)=>{
+        const APIkey='4sd68f3ikqg4'
+        fetch('https://api.ebird.org/v2/data/obs/recent/1/random'+regionCode,{
+        methods:"GET",
+        headers:{
+        'X-eBirdApiToken':APIkey
+        }
+         }) 
 
-    </header>
-    <main id="main-content">
-    </main> 
-
-    <script>
-    const triggerbutton=document.getElementById('fetch-button')
-    const mainContent = document.getElementById('main-content')
-
-  const fetchBirdData = ()=>{
-    console.log("run fetch")
-    var myHeaders = new Headers();
-    myHeaders.append("X-eBirdApiToken", "4sd68f3ikqg4");
-
-    var requestOptions = {
-      method: 'GET',
-      headers: myHeaders,
-      redirect: 'follow'
-    } 
-
-    fetch("https://api.ebird.org/v2/data/obs/KZ/recent?maxResults=1", requestOptions)
-      .then(response => response.json())
-      .then((result) => {
-        console.log(result[0].sciName)
-        appendBirdData(result, false)
-      })
-      .catch(error => console.log('error', error));
-
-
-        // const APIkey='4sd68f3ikqg4'
-        // fetch('https://api.ebird.org/v2/data/obs/recent/1/random',{
-        // methods:"GET",
-        // headers:{
-        // 'X-eBirdApiToken':APIkey
-        // }
-        //  }) 
-
-        // .then(respond=>respond.json())
-        // .then(data=>appendBirdData(data,false))
+        .then(respond=>respond.json())
+        .then(data=>appendBirdData(data,false))
    }
 
  const fetchSoundData=(requestName)=>{
         const APIkey='4b5575ce4be6324f3bfd1ac55b64a1a18e69b656'
-        fetch('https://xeno-canto.org/api/3/recordings&query=sp:"'+requestName.toLowerCase()+'"'+'&key='+APIkey)
-          .then(respond=>respond.json())
-          .then(data=>appendSoundData(data))
+        fetch('https://xeno-canto.org/api/3/recording&query=sp='+requestName,{
+        methods:'GET',
+        headers:{
+        'X-Api-Key':APIkey,
+        'Access-Control-Allow-Origin': "https://xueyi-mdp.github.io/"
+        }
+        })
+        .then(respond=>respond.json())
+        .then(data=>appendSoundData(data,false))
   }
   const fetchNasaData=(requestDate)=>{
         fetch('https://api.nasa.gov/planetary/apod?api_key=WPM5rPZF6xo3B1B4PBIjsX26ujgQ5f9TZZO8Y7vq&date='+requestDate)
@@ -82,22 +53,22 @@
   }
   const appendBirdData=(data,logData=true)=>{
         if(logData)console.log(data)
-          let birdData=data[0]
+          let birdData=data.bird[0]
         if(logData)console.log(birdData)
-          let birdName=birdData.sciName
+          let birdName=birdData.strComName
         
         let birdHTML=`
           <h2>${birdName}</h2>
           `
         mainContent.innerHTML=birdHTML
-        // const BirdSciName=new Name(birdData.dataModified)
-        // const Name=BirdSciName.getSciName()
-        // const soundName=Name
-        // console.log(soundName)
+        const BirdSciName=new Name(birdData.dataModified)
+        const Name=BirdSciName.getSciName()
+        const soundName=Name
+        console.log(soundName)
 
-        fetchSoundData(birdName)
+        fetchSoundData(soundName)
     }
-  const appendSoundData=(data,enterData=true)=>{
+const appendSoundData=(data,enterData=true)=>{
         if(enterData)console.log(data)
         let soundData=data.sound[0]
         if(enterData)console.log(soundData)
@@ -140,8 +111,3 @@ const appendNasaData=(data)=>{
         mainContent.innerHTML+=nasaHTML
 }
 }
-    triggerbutton.addEventListener("click",fetchBirdData)
-    </script>
-
-</body>
-</html>
